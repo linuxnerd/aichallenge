32ad70bdf8d249343ab0334677b9e1653e520363
Eric Lou <lwc_evale@hotmail.com>
2014-04-11 22:03:30 +0800

    Avoiding collisions

--- MyBot.rb
+++ MyBot.rb
@@ -7,16 +7,25 @@ ai.setup do |ai|
 	# your setup code here, if any
 end
 
+
+def do_move_direction ant, dir
+	new_loc = ant.square.neighbor(dir)
+	if !@orders.include?(new_loc) && new_loc.land?
+		ant.order dir
+		@orders[new_loc] = ant.square
+		return true
+	end
+	return false
+end
+
 ai.run do |ai|
 	# your turn code here
-	
+	@orders = {}
+
 	ai.my_ants.each do |ant|
 		# try to go north, if possible; otherwise try east, south, west.
 		[:N, :E, :S, :W].each do |dir|
-			if ant.square.neighbor(dir).land?
-				ant.order dir
-				break
-			end
+			break if do_move_direction(ant, dir)
 		end
 	end
 end
